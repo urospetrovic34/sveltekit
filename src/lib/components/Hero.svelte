1156545369e437a9ac10ca04068caaecac291723
uros.petrovic <uros.petrovic@quantox.com>
2024-01-06 01:34:42 +0100

    Hero Banner

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	const src =
+		'https://a.storyblok.com/f/266369/626x417/9472c3fd39/geometric-groovy-seamless-pattern_23-2148825765.webp';
+</script>
+
+<div class="flex">
+	<div class="flex flex-col gap-4 items-center justify-center flex-1 bg-slate-300">
+		<h1 class="text-4xl font-bold">Whispering Zephyr</h1>
+		<p class="text-center">
+			The ethereal glow of fireflies danced in harmony with the night's gentle breeze.
+		</p>
+		<a href="/ponder">
+			<button class="border border-slate-500 py-2 px-4 rounded-sm">Visit Store</button>
+		</a>
+	</div>
+	<div class="flex flex-1">
+		<img {src} alt="Hero Banner" />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	import Hero from '$lib/components/Hero.svelte';
+</script>
+
 <h1 class="fun">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="mb-3">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Hero />
 
 <style lang="postcss">
 	.fun {
